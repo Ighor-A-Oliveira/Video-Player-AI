1b7fb9f6912fc8834cb147681c6f3dace563f3be
Ighor A. de Oliveira <ighor.oliver1999@gmail.com>
2026-07-03 00:25:39 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        // Versões compatíveis com o Gradle 8.5
         classpath("com.android.tools.build:gradle:8.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
