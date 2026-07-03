f77d093b7c81f20ab5dd923187a7d80770f37a1d
Ighor A. de Oliveira <ighor.oliver1999@gmail.com>
2026-07-03 00:17:58 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/animeplayer/Models.kt
@@ -0,0 +1,22 @@
+package com.example.animeplayer
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Episode(
+    val uriString: String,
+    val name: String,
+    val seriesName: String,
+    val index: Int
+) : Parcelable
+
+data class Season(
+    val name: String,
+    val episodes: List<Episode>
+)
+
+data class Series(
+    val name: String,
+    val seasons: List<Season>
+)s
